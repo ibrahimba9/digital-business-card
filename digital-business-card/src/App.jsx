77fa1a102206a597d6358f22d7be4e4122c69a5b
unknown <ibrahim.benabdallah@esprit.tn>
2024-03-03 20:43:24 +0100

    add components

--- digital-business-card/src/App.jsx
+++ digital-business-card/src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import PersonalDetails from './components/PersonalDetails';
+import About from './components/About';
+import Skills from './components/Skills';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +13,9 @@ function App() {
   return (
     <>
       <h1>Hello! This is my <strong>Digital Business card</strong> project with ReactJS!</h1>
+      <PersonalDetails />
+      <About />
+      <Skills />
     </>
   )
 }
